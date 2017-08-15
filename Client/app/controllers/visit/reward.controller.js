ab81c965eb7478e546577eab2df66dccfb2e4818
liubin <1360866816@qq.com>
2017-08-15 17:27:43 +0800

    reset merge

--- Client/app/controllers/visit/reward.controller.js
+++ Client/app/controllers/visit/reward.controller.js
@@ -7,6 +7,17 @@ define([], function () {
 	    ViewModelName: 'rewardViewModel',
 	    ServiceContent: ['$scope', function ($scope) {
 	    	var $model = $scope.$model;
+	    	//客户返现设置
+			$("#award").off().on('click', function (e) {
+			   var award=$(".money").val();
+			   $model.getAward({
+				   	percent: award
+			   }).then(function (res) {
+				   $scope.awardData = res.data.data || [];
+				   $scope.$apply();
+				   res.data.seller_reward_percent=res.data.seller_reward_percent*100;
+			   });
+			});
 	    	//客户活动介绍
 		   	$("#intrSave").off().on('click', function (e) {
 		   	   var introduction=$("#textintro").val();
@@ -18,58 +29,28 @@ define([], function () {
 			   });
 			});
 			//开启关闭按钮
-			$(function() {
-				var index=1;
-			     $(".openButton").click(function() {
-			         $(this).css("backgroundColor","#6db1fb");
-			         $(".closeButton").css("backgroundColor","#fff")
-			         index=1;
-			     	$("#free-numble").removeAttr("disabled"); 
-			     });
-			     $(".closeButton").click(function() {
-			         $(this).css("backgroundColor","#6db1fb");
-			         $(".openButton").css("backgroundColor","#fff")
-			   		 index=0;
-			     	$("#free-numble").attr("disabled","disabled"); 
-			     });
-			     
-			});
-		   	//客户返现设置
-			$("#award").off().on('click', function (e) {
-			   var award=$(".money").val();
-			   // alert(award);
-			   $model.getAward({
-				   	percent: award
-			   }).then(function (res) {
-				   $scope.awardData = res.data.data || [];
-				   $scope.$apply();
-			   });
-			});
-			//维码设置历史记录
 			
-			$("#freestore").off().on('click', function (e) {
-				 $scope.his= $model.$his.data.data;
-				$("#content-reward").css("opacity","0.5")
-				 $($scope.his).each(function(index){
- 					var time=getLocalTime($scope.his[index].ctime);
-					$("#bbb").append("<p>"+time+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;开启&nbsp;&nbsp;免费名额"+$scope.his[index].qrFreeNum+"人</p>");
-				 })
-				 	// console.dir($scope.his[i]);
-				 	// $("#bbb").html($scope.his[i].ctime);
-				 $("#aaa").css("z-index","999");
-				 $("#pleaseColse").on('click',function(){
-				 	 $("#aaa").hide();
-				 	 $("#content-reward").css("opacity","1")
-				 })
-			});
+			var index;
+			$scope.openSet=function(){
+			    $(".openButton1").css("backgroundColor","#6db1fb");
+			    $(".closeButton1").css("backgroundColor","#fff")
+			    index=1;
+			    $("#free-numble").removeAttr("disabled"); 
+			};
+			$scope.closeSet=function(){
+			    $(".closeButton1").css("backgroundColor","#6db1fb");
+			    $(".openButton1").css("backgroundColor","#fff")
+			   	index=0;
+				$("#free-numble").attr("disabled","disabled"); 
+			} 			
 			//免费二维码设置
 			$("#numbleSave").off().on('click', function (e) {
 					var freeNum=$(".numble input").val();
 					var qrExpenses=$("#copyMoney").val();
 			    	$model.getQr({
 			    		isFree:index,
-			    		freeNum: freeNum,
-			    		qrExpenses:qrExpenses
+			    		freeNum: freeNum || '',
+			    		qrExpenses:qrExpenses || ''
 				    }).then(function (res) {
 				    	$scope.tixianData = res.data.data || [];
 				    	$scope.$apply();
