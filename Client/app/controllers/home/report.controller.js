e5019098f43de905f5629725eb94b3ba87f93d8c
jinlinyong <617018289@qq.com>
2017-08-22 20:27:28 +0800

    下载

--- Client/app/controllers/home/report.controller.js
+++ Client/app/controllers/home/report.controller.js
@@ -192,16 +192,99 @@ define([], function () {
                 // var ctxName=pathName.substring(0,pathName.substr(1).indexOf('/')+1);
                 if (a === 1) {
                     var statTime = $scope.obj.statTime;
-                    var activityName = $scope.obj.activityName;
+                    var activityName = encodeURI(encodeURI($scope.obj.activityName));
                     var productSn = $scope.obj.productSn;
-                    window.location.href = '/fixatreport/importExcelWinUseProvData?staTime=' + statTime + '&activityName=' + encodeURI(encodeURI(activityName)) + '&productSn=' + productSn;
+                    var url = "/fixatreport/importExcelWinUseProvData";
+                    var xhr = new XMLHttpRequest();
+                    var formData = new FormData();
+                    formData.append('statTime', statTime);
+                    formData.append('activityName', activityName);
+                    formData.append('productSn', productSn);                    
+                    xhr.overrideMimeType("text/plain; charset=x-user-defined");
+                    xhr.open('POST', url, true);
+                    xhr.responseType = "blob";
+                    xhr.responseType = "arraybuffer";
+                    xhr.setRequestHeader("token", sessionStorage.getItem('access_token'));
+                    xhr.setRequestHeader("loginId", sessionStorage.getItem('access_loginId'));
+                    xhr.onload = function(res) {
+                        if (this.status == 200) {
+                            var blob = new Blob([this.response], {type: 'application/vnd.ms-excel'});
+                            var respHeader = xhr.getResponseHeader("Content-Disposition");
+                            var fileName = decodeURI(respHeader.match(/filename=(.*?)(;|$)/)[1]);
+                            if (window.navigator.msSaveOrOpenBlob) {
+                                navigator.msSaveBlob(blob, fileName);
+                            } else {
+                                var link = document.createElement('a');
+                                link.href = window.URL.createObjectURL(blob);
+                                link.download = fileName;
+                                link.click();
+                                window.URL.revokeObjectURL(link.href);
+                            }
+                        }
+                    };
+                    xhr.send(formData);
+                    // window.location.href = '/fixatreport/importExcelWinUseProvData?staTime=' + statTime + '&activityName=' + encodeURI(encodeURI(activityName)) + '&productSn=' + productSn;
                 } else if (a === 2) {
                     var statTime = $scope.saoobj.statTime;
-                    var activityName = $scope.saoobj.activityName;
-                    window.location.href = '/fixatreport/importExcelScanUseProvData?staTime=' + statTime + '&activityName=' + encodeURI(encodeURI(activityName))
+                    var activityName = encodeURI(encodeURI($scope.saoobj.activityName));
+                    var url = "/fixatreport/importExcelScanUseProvData";
+                    var xhr = new XMLHttpRequest();
+                    var formData = new FormData();
+                    formData.append('statTime', statTime);
+                    formData.append('activityName', activityName);
+                    xhr.overrideMimeType("text/plain; charset=x-user-defined");
+                    xhr.open('POST', url, true);
+                    xhr.responseType = "blob";
+                    xhr.responseType = "arraybuffer";
+                    xhr.setRequestHeader("token", sessionStorage.getItem('access_token'));
+                    xhr.setRequestHeader("loginId", sessionStorage.getItem('access_loginId'));
+                    xhr.onload = function(res) {
+                        if (this.status == 200) {
+                            var blob = new Blob([this.response], {type: 'application/vnd.ms-excel'});
+                            var respHeader = xhr.getResponseHeader("Content-Disposition");
+                            var fileName = decodeURI(respHeader.match(/filename=(.*?)(;|$)/)[1]);
+                            if (window.navigator.msSaveOrOpenBlob) {
+                                navigator.msSaveBlob(blob, fileName);
+                            } else {
+                                var link = document.createElement('a');
+                                link.href = window.URL.createObjectURL(blob);
+                                link.download = fileName;
+                                link.click();
+                                window.URL.revokeObjectURL(link.href);
+                            }
+                        }
+                    };
+                    xhr.send(formData);
+                    // window.location.href = '/fixatreport/importExcelScanUseProvData?staTime=' + statTime + '&activityName=' + encodeURI(encodeURI(activityName))
                 } else if (a === 3) {
                     var statTime = $scope.summar.statTime;
-                    // console.log(statTime);
+                    var url = "/fixatreport/importExcelDailySummData";
+                    var xhr = new XMLHttpRequest();
+                    var formData = new FormData();
+                    formData.append('statTime', statTime);
+                    xhr.overrideMimeType("text/plain; charset=x-user-defined");
+                    xhr.open('POST', url, true);
+                    xhr.responseType = "blob";
+                    xhr.responseType = "arraybuffer";
+                    xhr.setRequestHeader("token", sessionStorage.getItem('access_token'));
+                    xhr.setRequestHeader("loginId", sessionStorage.getItem('access_loginId'));
+                    xhr.onload = function(res) {
+                        if (this.status == 200) {
+                            var blob = new Blob([this.response], {type: 'application/vnd.ms-excel'});
+                            var respHeader = xhr.getResponseHeader("Content-Disposition");
+                            var fileName = decodeURI(respHeader.match(/filename=(.*?)(;|$)/)[1]);
+                            if (window.navigator.msSaveOrOpenBlob) {
+                                navigator.msSaveBlob(blob, fileName);
+                            } else {
+                                var link = document.createElement('a');
+                                link.href = window.URL.createObjectURL(blob);
+                                link.download = fileName;
+                                link.click();
+                                window.URL.revokeObjectURL(link.href);
+                            }
+                        }
+                    };
+                    xhr.send(formData);
                     window.location.href = '/fixatreport/importExcelDailySummData?staTime=' + statTime
                 }
             }
