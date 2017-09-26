6d8bc36dffb9d9dc9f92305caad8325d26bc727c
liubin <1360866816@qq.com>
2017-09-26 11:21:16 +0800

    fix menu bugs

--- Client/app/directives/factorys/ngui.menu.directive.js
+++ Client/app/directives/factorys/ngui.menu.directive.js
@@ -8,10 +8,11 @@ define([], function () {
     var nguiMenu = angular.module('ngui.menu', []);
 
     var nguiMenuFn = function (util, $location) {
-        var defaults = { //默认配置
-            tpl: '/menu.tpl.html',
-            style: 'tab', //样式 tab,pill
-            tabs: [] //选项卡数组
+        //默认配置
+        var defaults = {
+            tpl: '/menu.tpl.html',  // 模板文件
+            style: 'tab',           //样式 tab,pill
+            tabs: []                //选项卡数组
         };
         var defineObj = { //指令定义对象
             restrice: 'AE',
@@ -38,30 +39,48 @@ define([], function () {
             // menu事件绑定
             scope.getActiveLi = function(e, node) {
                 scope.activeHash = node.menuCode;
+                sessionStorage.menuCode = node.menuCode || '';
             };
 
             // open close
             scope.clickTab = function ($event, tab) {
                 scope.activeCode = tab.menu.menuCode;
+                sessionStorage.menuCode = tab.menu.menuCode || '';
+                var $li = $($event.target).closest('li');
                 var tabs = scope.tabs || [];
-                if (tab.nodeList.length) {
-                    var $li = $($event.target).closest('li');
-                    if (tab.open) {
-                        $li.animate({
-                            height: '40px'
-                        }, 300, function () {
-                            tab.open = !tab.open;
-                            scope.$apply();
-                        });
+                _.each(tabs, function (t) {
+                    if (t.menu.menuCode === tab.menu.menuCode) {
+                        if (t.nodeList.length) {
+                            if (tab.open) {
+                                $li.animate({
+                                    height: '40px'
+                                }, 300, function () {
+                                    tab.open = !tab.open;
+                                    scope.$apply();
+                                });
+                            } else {
+                                $li.animate({
+                                    height: '' + (tab.nodeList.length+1) * 40 + 'px'
+                                }, 300, function () {
+                                    tab.open = !tab.open;
+                                    scope.$apply();
+                                });
+                            }
+                        }
                     } else {
-                        $li.animate({
-                            height: '' + (tab.nodeList.length+1) * 40 + 'px'
-                        }, 300, function () {
-                            tab.open = !tab.open;
-                            scope.$apply();
-                        });
+                        if (t.nodeList.length) {
+                            if (t.open) {
+                                $li.siblings().animate({
+                                    height: '40px'
+                                }, 300, function () {
+                                    t.open = false;
+                                    scope.$apply();
+                                });
+                                t.open = false;
+                            }
+                        }
                     }
-                }
+                });
             };
         }
 
@@ -72,16 +91,27 @@ define([], function () {
     nguiMenu.directive('repeatFinish', ['$timeout', function ($timeout) {
         return {
             link: function (scope, element, attr) {
+                var code = sessionStorage.menuCode;
                 if (scope.$last == true) {
                     $timeout(function () {
-                        // 默认取第一个
-                        var $li = $(".ui-navbar-left li")[0];
-                        if ($($li).find('a').hash) {
-                            $($li).find('a').trigger('click');
+                        var $target = $("[data-target='"+code+"']");
+                        if (code && $target.length) {
+                            var $child = $target.closest('.child');
+                            if ($child.length) {
+                                $($child).prev().trigger('click');
+                            }
+                            $target.trigger('click');
                         } else {
-                            $($li).find('a').trigger('click');
-                            $($($li).find('ul li a')[0]).trigger('click');
+                            // 默认取第一个
+                            var $li = $(".ui-navbar-left li")[0];
+                            if ($($li).find('a').hash) {
+                                $($li).find('a').trigger('click');
+                            } else {
+                                $($li).find('a').trigger('click');
+                                $($($li).find('ul li a')[0]).trigger('click');
+                            }
                         }
+
                     });
                 }
             }
