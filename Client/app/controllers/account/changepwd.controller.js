3d4dced2d1040b0d31a08d5f274930d989a03344
liubin <1360866816@qq.com>
2017-10-12 14:26:30 +0800

    bug fixed

--- Client/app/controllers/account/changepwd.controller.js
+++ Client/app/controllers/account/changepwd.controller.js
@@ -8,16 +8,44 @@ define([], function () {
         ServiceName: 'changePwdCtrl',
         ServiceType: 'controller',
         ViewModelName: 'changePwdViewModel',
-        ServiceContent: ['$scope', function ($scope) {
+        ServiceContent: ['$scope', 'authorization', function ($scope, auth) {
             var $model = $scope.$model;
-
+            var FIX_PWD = /(?!^(\d+|[a-zA-Z]+|[~!@#$%^&*?]+)$)^[\w~!@#$%\^&*?]{4,16}$/;
             var defaultVal = {
                 account: '',
                 newPwd: '',
                 reNewPwd: '',
-                error: false,
+                checkold: function (acc) {
+                    if (!acc) {
+                        $scope.oldpwd = true;
+                    } else {
+                        $scope.oldpwd = false;
+                    }
+                },
+                checkpwd: function (pwd) {
+                    if (FIX_PWD.test(pwd)) {
+                        $scope.newpwd = true;
+                    } else {
+                        $scope.newpwd = false;
+                    }
+                    if ($scope.reNewPwd && $scope.reNewPwd !== pwd) {
+                        $scope.nr = true;
+                    } else {
+                        $scope.nr = false;
+                    }
+                },
+                checkrepwd: function (pwd) {
+                    if ($scope.newPwd !== pwd) {
+                        $scope.nr = true;
+                    } else {
+                        $scope.nr = false;
+                    }
+                },
                 changepwd: function () {
-                    if ($scope.error) {
+                    this.checkold($scope.account);
+                    this.checkpwd($scope.newPwd);
+                    this.checkrepwd($scope.reNewPwd);
+                    if ($scope.oldpwd || $scope.newpwd || $scope.nr) {
                         return;
                     }
                     $model.changePwd({
@@ -25,11 +53,23 @@ define([], function () {
                         newPwd: $scope.newPwd
                     }).then(function (res) {
                         console.log(res);
+                        var data = res.data || {};
+                        if (data.ret === '500500') {
+                            console.log('失败');
+                            $("#tipsModalId .modal-body").html(data.message);
+                        } else {
+                            $(".icon-close").hide();
+                            $("#tipsModalId .modal-body").html('密码修改成功,请重新登陆!');
+                            setTimeout(function () {
+                                auth.logout();
+                            }, 3000);
+                        }
+                        $("#tipsModalId").modal('show');
                     });
                 },
                 closepage: function () {
                     //取消按钮
-                    console.log('closepage...');
+                    history.go(-1);
                 }
             };
             //默认值
--- Client/app/models/account/changepwd.model.js
+++ Client/app/models/account/changepwd.model.js
@@ -10,7 +10,6 @@ define([], function () {
         ServiceContent: ['request', function (request) {
             this.$model = function () {
                 var CHANGE_PWD_API = '/api/tztx/admin/user/modifyPwd';
-
                 // 修改密码
                 this.changePwd = function (params) {
                     return request.$Search(CHANGE_PWD_API, params, true);
