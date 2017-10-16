fc3aae19add516204addffee8d9db5b531f82836
liubin <1360866816@qq.com>
2017-10-16 11:52:23 +0800

    add new system

---
+++ Client/app/controllers/system/account/role.controller.js
@@ -0,0 +1,17 @@
+/**
+ * Author: {author}
+ * Create Date: 2017-10-16
+ * Description: role
+ */
+define([], function() {
+    var roleCtrl = {
+        ServiceType: "controller",
+        ServiceName: "roleCtrl",
+        ViewModelName: 'roleViewModel',
+        ServiceContent: ['$scope', function($scope) {
+            // TODO...
+        }]
+    };
+
+  return roleCtrl;
+});
---
+++ Client/app/controllers/system/customer/pay.controller.js
@@ -0,0 +1,17 @@
+/**
+ * Author: {author}
+ * Create Date: 2017-10-16
+ * Description: pay
+ */
+define([], function() {
+    var payCtrl = {
+        ServiceType: "controller",
+        ServiceName: "payCtrl",
+        ViewModelName: 'payViewModel',
+        ServiceContent: ['$scope', function($scope) {
+            // TODO...
+        }]
+    };
+
+  return payCtrl;
+});
